156aa66a2fd4c8fcd823464a915173862104d7fb
kkoryax <orachun.wsst@gmail.com>
2025-09-05 16:50:02 +0700

    connect(database): installed EF-Core, setup connectionstring and migration
    - installed EF-Core (Core, Tools, SQLServer)
    - appsettings add connectionstring from database
    - use PM migration to create table in database

---
+++ NoteFeature/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoteFeature.Models;
+
+namespace NoteFeature.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
