c794a43dc800bad73ee1f1e55d186038da7720f1
kkoryax <orachun.wsst@gmail.com>
2025-09-08 09:14:26 +0700

    method(Controller): added Edit Action and minor changed bug in form view
    - added Edit method in Contoller
    - fixed human-error bug in form (wrong typing text)

--- NoteFeature/Controllers/NoteController.cs
+++ NoteFeature/Controllers/NoteController.cs
@@ -33,5 +33,28 @@ namespace NoteFeature.Controllers
 
             return RedirectToAction("Index");
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Notes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Note obj)
+        {
+            _db.Notes.Update(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
