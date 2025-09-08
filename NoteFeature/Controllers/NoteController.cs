256f177aa697cf89e7615000caee59fc9a51addd
kkoryax <orachun.wsst@gmail.com>
2025-09-08 14:46:13 +0700

    feat(Controller): created Detail action, updated logic in Controller and changed model setting
    - added Detail action method, Detail View
    - changed Note Model (UpdatedAt from non-nullable to nullable)
    - updated logic in Controller to not created data in updatedAt to null

--- NoteFeature/Controllers/NoteController.cs
+++ NoteFeature/Controllers/NoteController.cs
@@ -28,8 +28,12 @@ namespace NoteFeature.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Note obj)
         {
-            _db.Notes.Add(obj);
-            _db.SaveChanges();
+            if (ModelState.IsValid)
+            {
+        
+                _db.Notes.Add(obj);
+                _db.SaveChanges();
+            }
 
             return RedirectToAction("Index");
         }
@@ -51,12 +55,15 @@ namespace NoteFeature.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(Note obj)
         {
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                return View(obj);
+                obj.UpdatedAt = DateTime.Now;
+                _db.Notes.Update(obj);
+
+                _db.Entry(obj).Property(x => x.CreatedAt).IsModified = false;
+
+                _db.SaveChanges();
             }
-            _db.Notes.Update(obj);
-            _db.SaveChanges();
 
             return RedirectToAction("Index");
         }
@@ -75,6 +82,19 @@ namespace NoteFeature.Controllers
             _db.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+        public IActionResult Detail(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Notes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
         }
     }
 }
--- NoteFeature/Models/NoteModel.cs
+++ NoteFeature/Models/NoteModel.cs
@@ -6,15 +6,15 @@ namespace NoteFeature.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required(ErrorMessage = "กรุณาใส่ชื่อเรื่อง")]
-        [StringLength(100, ErrorMessage = "ชื่อเรื่องต้องไม่เกิน 100 ตัวอักษร")]
-        [Display(Name = "ชื่อเรื่อง")]
+        [Required(ErrorMessage = "please give a title name")]
+        [StringLength(100, ErrorMessage = "title must not exceed 100 characters")]
+        [Display(Name = "title")]
         public string Title { get; set; }
-        [Required(ErrorMessage = "กรุณาใส่เนื้อหา")]
-        [StringLength(2000, ErrorMessage = "เนื้อหาต้องไม่เกิน 2000 ตัวอักษร")]
-        [Display(Name = "เนื้อหา")]
+        [Required(ErrorMessage = "please give a content")]
+        [StringLength(2000, ErrorMessage = "content must not exceed 2000 characters")]
+        [Display(Name = "content")]
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
